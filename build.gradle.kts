a39d9f878ae77463747814afc364641ff7ea6c98
Baaasty <46427883+Baaasty@users.noreply.github.com>
2023-06-01 23:59:30 +0200

    Add graphql to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     //implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
     //implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
